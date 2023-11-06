ff991a755735f884545dd69f3637429943ba1ce1
Martin Varmuza <mvarmuza@gmail.com>
2023-11-06 14:28:26 +0100

    fix(suite): list cardanoGetPublicKey in connect wrapped methods

--- suite-common/connect-init/src/connectInitThunks.ts
+++ suite-common/connect-init/src/connectInitThunks.ts
@@ -60,6 +60,7 @@ export const connectInitThunk = createThunk(
             'rippleGetAddress',
             'cardanoGetAddress',
             'solanaGetAddress',
+            'cardanoGetPublicKey',
             'applySettings',
             'changePin',
             'pushTransaction',
